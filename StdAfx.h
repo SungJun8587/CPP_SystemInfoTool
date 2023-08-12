b25749777955ea68b7cb9b927c199a265e1e351e
SungJun-An <islove8587@gmail.com>
2023-08-12 14:44:49 +0900

    Init

---
+++ StdAfx.h
@@ -0,0 +1,32 @@
+
+//***************************************************************************
+// stdafx.h : include file for standard system include files
+//
+//***************************************************************************
+
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <atlbase.h>
+#include <comdef.h>
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <Shellapi.h>
+#include <strsafe.h>
+#include <locale.h>
+
+#pragma warning(disable : 4251 4800)
+
+//***************************************************************************
+// customized or added header Files for the SystemInfoTool
+#include "SystemInfoToolAll.h"
+//***************************************************************************
+
+#endif // ndef __STDAFX_H__
---
+++ SystemInfoToolAll.h
@@ -0,0 +1,31 @@
+
+//***************************************************************************
+// This File includes customized or added header Files for the SystemInfoTool
+//***************************************************************************
+
+#ifndef __SYSTEMINFOTOOLALL_H__
+#define __SYSTEMINFOTOOLALL_H__
+
+#include <iostream>
+using namespace std;
+
+//***************************************************************************
+// Version Define
+//***************************************************************************
+#include <Common.h>
+#include <WebCommon.h>
+#include <SystemInfoCommon.h>
+#include <MemBuffer.h>
+#include <StringUtil.h>
+#include <Regular.h>
+#include <BaseFile.h>
+#include <EventLog.h>
+#include <BaseLinkedList.h>
+#include <OsInfo.h>
+#include <SoftwareInfo.h>
+#include <CpuInfo.h>
+#include <HardwareInfo.h>
+
+#define  _DEBUGLOG				  // Enable Logging For Monitoring System
+
+#endif // ndef __SYSTEMINFOTOOLALL_H__
